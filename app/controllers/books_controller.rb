84ce97d283b0db5d2364e9bf9d901295883423be
Gemma Goddard <122994950+gemmagoddard3@users.noreply.github.com>
2024-09-03 21:55:07 -0500

    Adding new database entries
    Updating the display to include new book properties

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -51,6 +51,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :price, :author, :published_date)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_04_015100) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_04_024919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.string "author"
+    t.date "published_date"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
